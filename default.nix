447cd5c13935a1fcdfcaf878d900070472d0265f
Jona Neef <Jona.Neef.97@gmail.com>
2023-07-12 13:35:37 +0000

    reset all

--- default.nix
+++ default.nix
@@ -1,21 +1,5 @@
-let
-  pinnedNixpkgs = import ./pinned-nixpkgs.nix;
-
-  hello = pkgs.writeShellScriptBin "hello" ''
-    echo "Hello from the Nix channel overlay 😀!"
-  '';
-
-  goodbye = pkgs.writeShellScriptBin "goodbye" ''
-    echo "So sad that you must go 😕!"
-  '';
-
-  overlays = [
-    (self: super: {
-      inherit goodbye hello;
-    })
-  ];
-
-  pkgs = pinnedNixpkgs {
-    inherit overlays;
-  };
-in pkgs
+{ pkgs }: {
+    deps = [
+        pkgs.cowsay
+    ];
+}
